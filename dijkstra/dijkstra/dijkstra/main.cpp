f0c6235e432711126bd2002d9b17d473bdf15046
zakattak <zachary.jay.crabtree@gmail.com>
2016-02-12 08:55:58 -0800

    initial

---
+++ dijkstra/dijkstra/dijkstra/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  dijkstra
+//
+//  Created by Zak Crabtree on 2/12/16.
+//  Copyright © 2016 Zach Crabtree. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, const char* argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
